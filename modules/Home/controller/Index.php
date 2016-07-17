790eb37e50d96e84ed5d8c14e52285dda9470bd5
Tomart91 <tomek.kur14@op.pl>
2016-07-17 20:56:54 +0200

    Removed code

--- modules/Home/controller/Index.php
+++ modules/Home/controller/Index.php
@@ -2,14 +2,9 @@
 
 namespace modules\Home\controller;
 
-use \core\database\Database;
-
 class Index extends \core\controller\ClientController {
 
 	public function process(\core\Request $request) {
-		$db = Database::getInstance();
-		$result = $db->select(['username', 'pass'])->from('users')->where('id = ?', [1])->orderBy('id', 'ASC')->limit(1)->toDo();
-				$result = $db->select(['username', 'pass'])->from('users')->where('id = ?', [1])->orderBy('id', 'ASC')->limit(1)->toDo();
 		$viewer = \core\Viewer::getInstance($request);
 		$viewer->view('Index.twig');
 	}
